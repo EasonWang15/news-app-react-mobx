60b36bef645111392fae4e44817a401a379ff158
Eason <13709310753@163.com>
2019-08-05 14:49:00 +0800

    更新配置

---
+++ build/precache-manifest.449d63fade79639ffb1c31ac05e826a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031fd3f2572a4d50306f42d41c20f757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf3772754790033faa5",
+    "url": "/static/css/main.eb99e06c.chunk.css"
+  },
+  {
+    "revision": "99d07972a15566121389",
+    "url": "/static/js/2.46357525.chunk.js"
+  },
+  {
+    "revision": "9cf3772754790033faa5",
+    "url": "/static/js/main.9638f74b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
